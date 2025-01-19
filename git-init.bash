35890ded23d6f707850fe19723c2fc03d4f2d7be
Debian at HA <debian.loureiro@gmail.com>
2025-01-19 17:59:10 +0100

    aliases

--- git-init.bash
+++ git-init.bash
@@ -6,8 +6,8 @@ git add README.md
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/ha-mon-lou/homeAssistant.git
+git push --set-upstream https://ha-mon-lou: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@github.com/ha-mon-lou/homeAssistant.git main
 git push -u origin main
-
 
 
 
