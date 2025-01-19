cbafe931b3c8f2954770d798c406e6e29a91e768
Debian at HA <debian.loureiro@gmail.com>
2025-01-19 17:46:19 +0100

    aliases

---
+++ git-init.bash
@@ -0,0 +1,13 @@
+echo "# homeAssistant" >> README.md
+git init
+git config --global user.email "debian.loureiro@gmail.com"
+git config --global user.name "Debian at HA"
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ha-mon-lou/homeAssistant.git
+git push -u origin main
+
+
+
+
